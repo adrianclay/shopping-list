eb86de7b420960a16eb78deecf4da20e893c0ea5
Adrian Clay <adieclay@gmail.com>
2020-05-28 22:29:05 +0100

    Add optional shoppingList prop to the ItemList component
    This will need to be passed through to the shoppingListFetcher,
    but for now just add the prop.

--- src/ItemList/index.tsx
+++ src/ItemList/index.tsx
@@ -1,16 +1,17 @@
 import React, {useEffect, useState} from "react";
 import ShoppingListItem from '../domain/ShoppingListItem';
+import ShoppingList from "../domain/ShoppingList";
 
 interface ShoppingListItemFetcher {
   subscribeToItemChanges(onUpdate: (items: ShoppingListItem[]) => void, onError: () => void): () => void;
 }
 
-interface ItemListProps {
-  shoppingListItemFetcher: ShoppingListItemFetcher
+export interface ItemListProps {
+  shoppingList?: ShoppingList;
 }
 
 function ItemListConstructor(shoppingListItemFetcher: ShoppingListItemFetcher) {
-  return function ItemList() {
+  return function ItemList(_ : ItemListProps) {
     const [isLoading, setIsLoading] = useState(true);
     const [fetchErrored, setFetchError] = useState(false);
     const [shoppingListItems, setShoppingListItems] = useState([] as ShoppingListItem[]);
