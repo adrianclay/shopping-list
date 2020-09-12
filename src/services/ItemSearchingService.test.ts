10c47a78653ced97169872fa6a2e8c73eea8d5d0
Adrian Clay <adieclay@gmail.com>
2020-09-12 19:25:16 +0100

    Fix TypeScript errors in ItemSearchingService.test.ts

--- src/services/ItemSearchingService.test.ts
+++ src/services/ItemSearchingService.test.ts
@@ -23,40 +23,42 @@ beforeEach(() => {
 });
 
 describe('adding an item', () => {
+  const addShoppingListItem = (name: string) => itemSearchingService.addShoppingListItem({ name, list: shoppingListDummy });
+
   test('with one single letter word name', () => {
-    itemSearchingService.addShoppingListItem({ name: 'C' });
+    addShoppingListItem('C');
     expect(itemStoreSpy.addShoppingListItem).toHaveBeenLastCalledWith(expect.objectContaining({ search_queries: ['c'] }));
   });
 
   test('with one two letter word name', () => {
-    itemSearchingService.addShoppingListItem({ name: 'Ca' });
+    addShoppingListItem('Ca');
     expect(itemStoreSpy.addShoppingListItem).toHaveBeenLastCalledWith(expect.objectContaining({ search_queries: ['c', 'ca'] }));
   });
 
   test('with one three letter word name', () => {
-    itemSearchingService.addShoppingListItem({ name: 'Can' });
+    addShoppingListItem('Can');
     expect(itemStoreSpy.addShoppingListItem).toHaveBeenLastCalledWith(expect.objectContaining({ search_queries: ['c', 'ca', 'can'] }));
   });
 
   test('with one eleven letter word name', () => {
-    itemSearchingService.addShoppingListItem({ name: 'Candlestick' });
+    addShoppingListItem('Candlestick');
     expect(itemStoreSpy.addShoppingListItem).toHaveBeenLastCalledWith(expect.objectContaining({ search_queries: ['c', 'ca', 'can', 'cand', 'candl'] }));
   });
 
   test('with two one letter words name', () => {
-    itemSearchingService.addShoppingListItem({ name: 'a z' });
+    addShoppingListItem('a z');
     expect(itemStoreSpy.addShoppingListItem).toHaveBeenLastCalledWith(expect.objectContaining({ search_queries: ['a', 'z'] }));
   });
 
   test('with leading and trailing spaces', () => {
-    itemSearchingService.addShoppingListItem({ name: ' a  z ' });
+    addShoppingListItem(' a  z ');
     expect(itemStoreSpy.addShoppingListItem).toHaveBeenLastCalledWith(expect.objectContaining({ search_queries: ['a', 'z'] }));
   });
 });
 
 describe('updating an item', () => {
   test('with one five letter word', () => {
-    itemSearchingService.updateItem({ name: 'Cream' });
+    itemSearchingService.updateItem({ name: 'Cream', list: shoppingListDummy, id: '100' });
     expect(itemStoreSpy.updateItem).toHaveBeenLastCalledWith(expect.objectContaining({ search_queries: ['c', 'cr', 'cre', 'crea', 'cream'] }));
   });
 });
