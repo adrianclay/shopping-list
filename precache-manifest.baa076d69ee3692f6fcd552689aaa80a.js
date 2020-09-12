2003f6c3f9eee22893823a5ea2c9b79446a74372
adrianclay <adieclay@gmail.com>
2020-09-12 16:17:27 +0000

    Deploying to gh-pages from  @ bc01d2a5c6bfb8349588e4eccba1d202e2c40ded 🚀

---
+++ precache-manifest.baa076d69ee3692f6fcd552689aaa80a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acc814bea2416e5027993e956df3b60",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6d24ae16a63ce4b38da8.bundle.js"
+  },
+  {
+    "url": "runtime~main.6d24ae16a63ce4b38da8.bundle.js"
+  },
+  {
+    "url": "vendors~main.6d24ae16a63ce4b38da8.bundle.js"
+  },
+  {
+    "url": "vendors~main.6d24ae16a63ce4b38da8.bundle.js.LICENSE.txt"
+  }
+]);
