b97959853839050b23b5f07f3f1a23a86ff94dfc
Adrian Clay <adieclay@gmail.com>
2020-10-07 18:50:28 +0100

    Use the AddToShoppingList use case in App

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,9 @@ import { Container } from 'semantic-ui-react'
 import AlphaBanner from './AlphaBanner';
 import LoginConstructor, { Authenticator } from './Login';
 import { _createShoppingList, _listShoppingLists } from './services/Firestore/ShoppingLists';
-import { searchForItems, searchingAddShoppingListItem, searchingUpdateItem } from './services/ItemSearchingService';
-import { _addShoppingListItem, _deleteShoppingListItem, _listShoppingListItems, _readdShoppingListItem, _searchForItems, _saveShoppingListItem } from './services/Firestore/ShoppingListItems';
+import { searchForItems, searchingUpdateItem } from './services/ItemSearchingService';
+import { _deleteShoppingListItem, _listShoppingListItems, _readdShoppingListItem, _searchForItems, _saveShoppingListItem } from './services/Firestore/ShoppingListItems';
+import _AddToShoppingList from './use_cases/AddToShoppingList';
 
 function AppConstructor(authenticator: Authenticator, firebase: firebase.app.App) {
   const firestore = firebase.firestore();
@@ -20,7 +21,7 @@ function AppConstructor(authenticator: Authenticator, firebase: firebase.app.App
   const EditItemForm = EditItemFormConstructor(searchingUpdateItem(_saveShoppingListItem(firebase.firestore())));
   const ShoppingListViewer = ShoppingListViewerConstructor(
     ListSelectorConstructor(_listShoppingLists(firestore)),
-    AddItemFormConstructor(_readdShoppingListItem(firestore), searchingAddShoppingListItem(_addShoppingListItem(firestore)), searchForItems(_searchForItems(firestore))),
+    AddItemFormConstructor(_readdShoppingListItem(firestore), _AddToShoppingList(searchingUpdateItem(_saveShoppingListItem(firestore))), searchForItems(_searchForItems(firestore))),
     ItemListConstructor(_listShoppingListItems(firestore), _deleteShoppingListItem(firestore), EditItemForm),
     CreateShoppingListFormConstructor(_createShoppingList(firestore))
   );
