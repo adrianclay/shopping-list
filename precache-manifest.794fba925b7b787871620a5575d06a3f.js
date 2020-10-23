13b4c9f0d964326c8392e5eac8eea21701d75de4
adrianclay <adieclay@gmail.com>
2020-10-23 18:55:29 +0000

    Deploying to gh-pages from  @ 136da1442660e8a27bcce9541d2c75c60ba3039e 🚀

---
+++ precache-manifest.794fba925b7b787871620a5575d06a3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b74badaed755a86144241bb9a55c17",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1866d07eb7bfe4fc78ad.bundle.js"
+  },
+  {
+    "url": "runtime~main.1866d07eb7bfe4fc78ad.bundle.js"
+  },
+  {
+    "url": "vendors~main.1866d07eb7bfe4fc78ad.bundle.js"
+  },
+  {
+    "url": "vendors~main.1866d07eb7bfe4fc78ad.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9d96aae7506aeda659a3681877f66fc7.js"
+  "precache-manifest.794fba925b7b787871620a5575d06a3f.js"
 );
 
 self.addEventListener('message', (event) => {
