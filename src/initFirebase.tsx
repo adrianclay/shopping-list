eb30d311dbceffb40e9ffd6152e4b173f2bcf136
Adrian Clay <adieclay@gmail.com>
2021-05-08 19:24:55 +0100

    Remove support for connecting to a real Firebase Auth in development
    Can easily add it back, but for now it adds unnecessary complexity

--- src/initFirebase.tsx
+++ src/initFirebase.tsx
@@ -4,20 +4,9 @@ import 'firebase/auth'
 
 export default async function initFirebase() {
     if (process.env.NODE_ENV === 'development') {
-        const firebaseConfig = {
+        const app = firebase.initializeApp({
             projectId: "dummy-project-id",
-        };
-        if (process.env.REACT_APP_FIREBASE_API_KEY && process.env.REACT_APP_FIREBASE_AUTH_DOMAIN) {
-            Object.assign(
-                firebaseConfig,
-                {
-                    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-                    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-                }
-            );
-        }
-
-        const app = firebase.initializeApp(firebaseConfig);
+        });
         app.firestore().settings({
             host: "localhost:8080",
             ssl: false
