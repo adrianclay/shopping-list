48dcb4af57d3b90f30eb542cc0fcf7716173ad01
Adrian Clay <adrian@madetech.com>
2021-10-21 11:44:31 +0100

    Resolve contrast accessibility issue
    WCAG 1.4.3 Contrast

--- src/AlphaBanner/index.tsx
+++ src/AlphaBanner/index.tsx
@@ -2,7 +2,7 @@ import { Label, Segment } from "semantic-ui-react";
 
 function AlphaBanner() {
   return <Segment color='blue' basic secondary>
-    <Label color='blue' style={{textTransform: 'uppercase', letterSpacing: '0.0625em'}} basic horizontal>Alpha</Label>
+    <Label color='black' style={{textTransform: 'uppercase', letterSpacing: '0.0625em'}} basic horizontal>Alpha</Label>
     This software is new
   </Segment>;
 }
