967f0fbad3cc12de42b855f4ef402b485e42fb2c
Adrian Clay <adrian@madetech.com>
2023-01-27 20:19:32 +0000

    Add additional Event to EventLogViewer component

--- src/ShoppingList/EventLogViewer/index.stories.tsx
+++ src/ShoppingList/EventLogViewer/index.stories.tsx
@@ -1,42 +1,49 @@
 
 import _EventLogViewer from ".";
-import ShoppingListEvent from "../../domain/ShoppingListEvent";
 import ShoppingListFactory from "../../factories/ShoppingList";
 import { days, hours, minutes } from "../../RelativeTime/periods";
 
 const list = ShoppingListFactory.build();
-const events : ShoppingListEvent[] = [
-  {
-    list,
-    type: 'item_added',
-    item: {
-      name: 'Peas',
-      id: 'peaz'
-    },
-    created_on: new Date(Date.now() - 3 * days)
-  },
-  {
-    list,
-    type: 'item_bought',
-    item: {
-      name: 'Pudding',
-      id: 'puddin'
-    },
-    created_on: new Date(Date.now() - 2 * hours)
-  },
-  {
-    list,
-    type: 'item_added',
-    item: {
-      name: 'Pudding',
-      id: 'puddin'
-    },
-    created_on: new Date(Date.now() - 15 * minutes)
-  }
-];
 
 const EventLogViewer = _EventLogViewer((_a, onUpdate, _b) => {
-  onUpdate(events);
+  onUpdate([
+    {
+      list,
+      type: 'item_added',
+      item: {
+        name: 'Peas',
+        id: 'peaz'
+      },
+      created_on: new Date(Date.now() - 3 * days)
+    },
+    {
+      list,
+      type: 'item_bought',
+      item: {
+        name: 'Pudding',
+        id: 'puddin'
+      },
+      created_on: new Date(Date.now() - 2 * hours)
+    },
+    {
+      list,
+      type: 'item_added',
+      item: {
+        name: 'Porridge',
+        id: 'porridge'
+      },
+      created_on: new Date(Date.now() - 15 * minutes)
+    },
+    {
+      list,
+      type: 'item_bought',
+      item: {
+        name: 'Ham',
+        id: 'ham'
+      },
+      created_on: new Date()
+    }
+  ]);
   return () => {}
 });
 
